18c9f335aae6ff92a9f6a1f836dffd106634da9f
khan <omeryagci3295@gmail.com>
2022-07-31 15:25:34 +0300

    toastify added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Contacts from "./components/contacts/Contacts";
 import FormComponent from "./components/form/FormComponent";
 import { useState } from "react";
 import { AddUser, UpdateUser } from "./utils/functions";
+import { ToastContainer } from "react-toastify";
 
 const initialValues = { username: "", phoneNumber: "", gender: "NO INFO" };
 
 function App() {
   const [info, setInfo] = useState(initialValues);
+  const [isAdd, setIsAdd] = useState("ADD");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,9 +19,11 @@ function App() {
       AddUser(info);
     }
     setInfo(initialValues);
+    setIsAdd("ADD");
   };
 
   const editUser = (id, username, phoneNumber, gender) => {
+    setIsAdd("UPDATE");
     setInfo({ id, username, phoneNumber, gender });
   };
   return (
@@ -28,8 +32,10 @@ function App() {
         info={info}
         setInfo={setInfo}
         handleSubmit={handleSubmit}
+        isAdd={isAdd}
       />
       <Contacts editUser={editUser} />
+      <ToastContainer />
     </div>
   );
 }
